47b746d46565b0a9c5e42210e2b69608455c5d1e
Evan James <sc18ej@leeds.ac.uk>
2021-11-01 17:45:39 +0000

    Used the t-midpoints instead of the t values of the intersections - Now working!

--- pixelwidget.cpp
+++ pixelwidget.cpp
@@ -13,8 +13,8 @@
 #include <algorithm>
 #include "pixelwidget.hpp"
 
-//#define LINE 2.99f, 50.0f, 50.0f, 2.99f
-#define LINE 14.0f, 1.0f, 50.0f, 60.0f
+#define LINE 2.999f, 49.999f, 49.999f, 2.999f
+//#define LINE 14.0f, 1.0f, 50.0f, 60.0f
 //#define LINE 0.5f, 0.5f, 2.5f, 2.5f
 //#define LINE 
 //#define LINE 0.0f, 69.0f, 69.0f, 0.0f
@@ -67,21 +67,22 @@ void PixelWidget::DrawLine(float x1, float y1, float x2, float y2)
     tValues.push_back(findIntersection(drawLine, horizontalLine));
   }
 
+  //Sort the vector of t values and then find the midpoints of the adjacent pairs.
+  //This midpoint yields a t value that is the midpoint of the line segment that
+  //passes through a pixel. These t values are better to use than those of the
+  //intersections since we do not have to deal with floating point inprecision when
+  //representing integers.
+
   std::sort(tValues.begin(), tValues.end());
 
   std::vector<float> tMidpoints;
-  
   for (int i = 0; i < tValues.size() - 1; i++)
     tMidpoints.push_back((tValues[i] + tValues[i + 1]) / 2.0f);
 
-  std::cout << "t values: " << std::endl;
-  for (int i = 0; i < tValues.size(); i++)
-    std::cout << tValues[i] << std::endl;
-
-  for (int i = 0; i < tValues.size(); i++)
+  for (int i = 0; i < tMidpoints.size(); i++)
   {
     ParameterisedLine drawLine = { { x1, y1 }, { x2, y2 } };
-    float t = tValues[i];
+    float t = tMidpoints[i];
     vec2 intersectionPoint = ((1 - t) * drawLine.p + t * drawLine.q) + std::numeric_limits<float>::epsilon();
     std::cout << "Pixel: (" << static_cast<int>(intersectionPoint.x) << ", " << static_cast<int>(intersectionPoint.y) << ")" << std::endl;
     SetPixel(static_cast<int>(intersectionPoint.x), static_cast<int>(intersectionPoint.y), { 255, 255, 255 });
@@ -245,8 +246,8 @@ void PixelWidget::paintEvent( QPaintEvent * )
   //DrawLine(14.0f, 1.0f, 50.0f, 60.0f);
   //SetPixel(2, 50, {255, 0, 0});
   //SetPixel(50, 2, {255, 0, 0});
+  DrawLinePerfect(LINE);
   DrawLine(LINE);
-  //DrawLinePerfect(LINE);
   //DrawLine(0.0f, 0.0f, 30.0f, 30.0f);
 
   /* DrawLine(0.0f, 0.0f, 2.0f, 2.0f);
